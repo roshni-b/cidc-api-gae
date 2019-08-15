d2940c7716de7ac6ac2a84869f1f122eb21ef974
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-08-15 12:56:23 -0400

    Add eve-sqlalchemy to cidc-api-modules deps

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ setup(
     install_requires=[
         "flask==1.1.1",
         "flask-sqlalchemy==2.4.0",
+        "eve-sqlalchemy==0.7.0",
         "google-cloud-storage==1.16.1",
     ],
     license="MIT license",
     packages=["cidc_api.config"],
     py_modules=["cidc_api.models"],
     url="https://github.com/CIMAC-CIDC/cidc_api-gae",
-    version="0.1.3",
+    version="0.1.4",
     zip_safe=False,
 )
