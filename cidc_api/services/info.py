64120594ed4024bb88af3f8586d1bd4b0875b2fe
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2019-09-13 16:16:40 -0400

    only supported assays in info

--- cidc_api/services/info.py
+++ cidc_api/services/info.py
@@ -2,11 +2,21 @@
 from flask import Blueprint, jsonify
 
 from cidc_schemas.template import _TEMPLATE_PATH_MAP
+from cidc_schemas import prism
+
 
 info_api = Blueprint("info", __name__, url_prefix="/info")
 
-assay_list = [k for k, v in _TEMPLATE_PATH_MAP.items() if "metadata" in v]
-manifest_list = [k for k, v in _TEMPLATE_PATH_MAP.items() if "manifests" in v]
+assay_list = [
+    k
+    for k, v in _TEMPLATE_PATH_MAP.items()
+    if "metadata" in v and k in prism.SUPPORTED_ASSAYS
+]
+manifest_list = [
+    k
+    for k, v in _TEMPLATE_PATH_MAP.items()
+    if "manifests" in v and k in prism.SUPPORTED_MANIFESTS
+]
 
 
 @info_api.route("assays", methods=["GET"])
