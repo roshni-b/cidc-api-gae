d15371f694cd7c09e6dd1b17f8bcbfbfee9dbdbc
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2019-09-16 13:03:37 -0400

    fix imports

--- cidc_api/services/info.py
+++ cidc_api/services/info.py
@@ -1,7 +1,6 @@
 """Endpoints providing info related to this API"""
 from flask import Blueprint, jsonify
 
-from cidc_schemas.template import _TEMPLATE_PATH_MAP
 from cidc_schemas import prism
 
 
@@ -11,10 +10,10 @@ info_api = Blueprint("info", __name__, url_prefix="/info")
 @info_api.route("assays", methods=["GET"])
 def assays():
     """List all supported assays"""
-    return jsonify(SUPPORTED_ASSAYS)
+    return jsonify(prism.SUPPORTED_ASSAYS)
 
 
 @info_api.route("manifests", methods=["GET"])
 def manifests():
     """List all supported manifests"""
-    return jsonify(SUPPORTED_MANIFESTS)
+    return jsonify(prism.SUPPORTED_MANIFESTS)
