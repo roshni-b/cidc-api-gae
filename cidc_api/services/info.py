d9505845e4e722ced024823c8da0a46138ed1b90
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2019-09-16 11:38:33 -0400

    simplified /info/{assays,manifests} with using just SUPPORTED_...

--- cidc_api/services/info.py
+++ cidc_api/services/info.py
@@ -7,25 +7,13 @@ from cidc_schemas import prism
 
 info_api = Blueprint("info", __name__, url_prefix="/info")
 
-assay_list = [
-    k
-    for k, v in _TEMPLATE_PATH_MAP.items()
-    if "metadata" in v and k in prism.SUPPORTED_ASSAYS
-]
-manifest_list = [
-    k
-    for k, v in _TEMPLATE_PATH_MAP.items()
-    if "manifests" in v and k in prism.SUPPORTED_MANIFESTS
-]
-
-
 @info_api.route("assays", methods=["GET"])
 def assays():
     """List all supported assays"""
-    return jsonify(assay_list)
+    return jsonify(SUPPORTED_ASSAYS)
 
 
 @info_api.route("manifests", methods=["GET"])
 def manifests():
     """List all supported manifests"""
-    return jsonify(manifest_list)
+    return jsonify(SUPPORTED_MANIFESTS)
