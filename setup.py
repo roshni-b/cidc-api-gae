b4235c753e9b3a6be58e9c16d74167d907bc6eec
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-09-24 16:51:18 -0400

    Bump module version

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
     packages=["cidc_api.config"],
     py_modules=["cidc_api.models"],
     url="https://github.com/CIMAC-CIDC/cidc_api-gae",
-    version="0.5.0",
+    version="0.5.1",
     zip_safe=False,
 )
