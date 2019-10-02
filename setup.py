612e003ff9a96247ba585d71b534d93f3c36b9c0
Pavel T <pavel@ds.dfci.harvard.edu>
2019-10-02 08:31:56 -0400

    version="0.6.2" (#106)

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     packages=["cidc_api.config"],
     py_modules=["cidc_api.models"],
     url="https://github.com/CIMAC-CIDC/cidc_api-gae",
-    version="0.6.1",
+    version="0.6.2",
     zip_safe=False,
 )
