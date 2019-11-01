e39b187438a1352d82df5b5b7a14f9b7659615bc
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2019-11-01 09:45:07 -0400

    env var renaming as per review

--- cidc_api/config/settings.py
+++ cidc_api/config/settings.py
@@ -21,7 +21,7 @@ TEMPLATES_DIR = path.join("/tmp", "templates")
 ## End application environment config
 
 ## Configure Dev CFn
-CFNS_HTTP = environ.get("CFNS_HTTP")
+DEV_CFUNCTIONS_SERVER = environ.get("DEV_CFUNCTIONS_SERVER")
 ## End Dev CFn Config
 
 secrets = get_secret_manager(TESTING)
