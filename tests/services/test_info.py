302c95609146b242be50d7684893ce648670b475
Roshni Biswas <rbiswas@ds.dfci.harvard.edu>
2019-11-06 09:31:24 -0500

    test analyses endpoint

--- tests/services/test_info.py
+++ tests/services/test_info.py
@@ -9,6 +9,14 @@ def test_info_assays(app_no_auth):
     assert "wes" in res.json
 
 
+def test_info_analyses(app_no_auth):
+    """Check that the /info/analyses endpoint returns a list of assays"""
+    client = app_no_auth.test_client()
+    res = client.get(f"{INFO_ENDPOINT}/analyses")
+    assert type(res.json) == list
+    assert "cytof_analysis" in res.json
+
+
 def test_info_manifests(app_no_auth):
     """Check that the /info/manifests endpoint returns a list of manifests"""
     client = app_no_auth.test_client()
