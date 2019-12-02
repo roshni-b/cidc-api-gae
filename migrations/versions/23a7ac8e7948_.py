b076a9a3e9e80331e94922d3161e8cc513e7f643
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-12-02 13:08:38 -0500

    Add a migration to trigger visualization preprocessing CFn (#174)

---
+++ migrations/versions/23a7ac8e7948_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 23a7ac8e7948
+Revises: 5f70e6f7c0d9
+Create Date: 2019-12-02 12:40:16.956281
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+from cidc_api.utils.migrations import republish_artifact_uploads
+
+# revision identifiers, used by Alembic.
+revision = "23a7ac8e7948"
+down_revision = "5f70e6f7c0d9"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Trigger visualization preprocessing
+    republish_artifact_uploads()
+
+
+def downgrade():
+    pass
