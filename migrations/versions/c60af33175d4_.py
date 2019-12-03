d198cfd5905594943d89c8f59e7b7b304f492716
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-12-03 11:58:22 -0500

    Add new preprocessing migration trigger (#175)

---
+++ migrations/versions/c60af33175d4_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: c60af33175d4
+Revises: 23a7ac8e7948
+Create Date: 2019-12-03 11:32:17.224586
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+from cidc_api.utils.migrations import republish_artifact_uploads
+
+# revision identifiers, used by Alembic.
+revision = "c60af33175d4"
+down_revision = "23a7ac8e7948"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Integrate in changes from https://github.com/CIMAC-CIDC/cidc-cloud-functions/pull/80
+    republish_artifact_uploads()
+
+
+def downgrade():
+    pass
