0a954d3004b5c389514370537936d347409eeb59
Roshni Biswas <rbiswas@ds.dfci.harvard.edu>
2019-12-20 14:09:22 -0500

    bump version=0.13.1

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     packages=["cidc_api.config"],
     py_modules=["cidc_api.models", "cidc_api.gcloud_client", "cidc_api.emails"],
     url="https://github.com/CIMAC-CIDC/cidc_api-gae",
-    version="0.13.0",
+    version="0.13.1",
     zip_safe=False,
 )
