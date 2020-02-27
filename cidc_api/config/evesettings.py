f139aef3ff476791f11509b8b2df8fe48340ecca
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-02-27 10:09:44 -0500

    Add option to publish to artifact_upload topic on downloadable_file creation (#232)
    * Add option to publish to artifact_upload topic on downloadable_file creation
    
    * Remove unnecessary record deletion in tests
    
    * Refactor settings module to prevent circular import
    
    * Move credentials initialization to evesettings.py

---
+++ cidc_api/config/evesettings.py
@@ -0,0 +1,38 @@
+import os
+import tempfile
+
+from cidc_api.config import db, secrets
+from cidc_api.models import get_DOMAIN
+
+TESTING = os.environ.get("TESTING")
+TEMPLATES_DIR = os.path.join("/tmp", "templates")
+
+## Configure database
+SQLALCHEMY_DATABASE_URI = db.get_sqlachemy_database_uri(TESTING)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+## End database config
+
+## Configure Eve REST API
+RESOURCE_METHODS = ["GET", "POST"]
+ITEM_METHODS = ["GET", "PATCH"]
+CACHE_CONTROL = "no-cache"
+DOMAIN = get_DOMAIN()
+PAGINATION_DEFAULT = 200
+PAGINATION_LIMIT = 200
+MEDIA_ENDPOINT = None
+## End Eve REST API config
+
+# Download the credentials file to a temporary file,
+# then set the GOOGLE_APPLICATION_CREDENTIALS env variable
+# to its path.
+#
+# NOTE: doing this shouldn't be necessary from within App Engine,
+# but for some reason, google.cloud.storage.Blob.generate_signed_url
+# fails with a credentials-related error unless this is explicitly
+# set.
+if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") and not TESTING:
+    secret_manager = secrets.get_secret_manager()
+    creds_file_name = tempfile.mktemp(".json")
+    with open(creds_file_name, "w") as creds_file:
+        creds_file.write(secret_manager.get("APP_ENGINE_CREDENTIALS"))
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = creds_file_name
