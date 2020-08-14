02986337527df6faf414c9507b56c029fc87c1b1
Roshni Biswas <rbiswas@ds.dfci.harvard.edu>
2020-08-14 10:53:47 -0400

    bump package versions

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     license="MIT license",
     packages=["cidc_api.config", "cidc_api.models", "cidc_api.shared"],
     url="https://github.com/CIMAC-CIDC/cidc_api-gae",
-    version="0.18.26",
+    version="0.18.27",
     zip_safe=False,
 )
