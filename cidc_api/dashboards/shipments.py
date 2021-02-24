5701210f10e77583c2bebe840e10a2d0d8ff51d2
Jacob Lurye <jlurye@jimmy.harvard.edu>
2021-02-24 16:09:35 -0500

    Convert upload jobs table into a shipments dashboard (#395)
    * Convert upload jobs table into a shipments dashboard
    
    * Add tests for shipments dashboard helpers
    
    * Flesh out shipments dashboard webdriver test

---
+++ cidc_api/dashboards/shipments.py
@@ -0,0 +1,188 @@
+from typing import List, Optional
+
+import pandas as pd
+import dash_table as dt
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output, Input
+from sqlalchemy.sql.expression import column
+
+from .dash_utils import create_new_dashboard
+from ..models import UploadJobStatus, UploadJobs, CIDCRole, TrialMetadata
+from ..shared.auth import requires_auth
+
+
+def get_manifest_samples(trial_id: str, manifest_id: str) -> Optional[pd.DataFrame]:
+    """Return a list of sample metadata associated with the given manifest upload."""
+    # Get the most recent manifest upload with the given trial and manifest ids.
+    # Although we're only querying for a single record, the UploadJobs.list method
+    # makes it easier to build our query.
+    upload_records: List[UploadJobs] = UploadJobs.list(
+        page_size=1,
+        sort_field="id",
+        sort_direction="desc",
+        filter_=lambda q: q.filter(
+            UploadJobs.status == UploadJobStatus.MERGE_COMPLETED.value,
+            UploadJobs.trial_id == trial_id,
+            # This relies on the fact any manifest upload's metadata_patch will have exactly
+            # one entry in its 'shipments' array corresponding to the manifest that was uploaded.
+            UploadJobs.metadata_patch[("shipments", "0", "manifest_id")].astext
+            == manifest_id,
+        ),
+    )
+
+    # Return an empty list if no matching manifests were found
+    if len(upload_records) == 0:
+        return None
+
+    upload_record = upload_records[0]
+
+    # Normalize the participant-sample metadata to get a dataframe of samples
+    metadata_patch = upload_record.metadata_patch
+    samples_df = pd.json_normalize(
+        metadata_patch["participants"], "samples", meta=["cohort_name"]
+    )
+    samples_df["assay_type"] = metadata_patch["shipments"][0]["assay_type"]
+    samples_df["cimac_participant_id"] = samples_df.cimac_id.apply(lambda x: x[0:7])
+    samples_df["manifest_id"] = manifest_id
+    samples_df["cidc_received"] = upload_record._created
+
+    return samples_df
+
+
+def get_trial_shipments(trial_id: str) -> Optional[List[dict]]:
+    """Return a list of distinct shipments associated with the given trial."""
+    trial_record = TrialMetadata.find_by_trial_id(trial_id)
+
+    # Return an empty list of shipments if the trial doesn't exist
+    if trial_record is None:
+        return None
+
+    # Extract and deduplicate shipments
+    seen = set()
+    shipments = []
+    for shipment in trial_record.metadata_json.get("shipments", []):
+        manifest_id = shipment["manifest_id"]
+
+        # Skip duplicates
+        if manifest_id in seen:
+            continue
+        seen.add(manifest_id)
+
+        # Get related samples
+        manifest_samples = get_manifest_samples(trial_id, manifest_id)
+
+        # Add or reformat fields
+        shipment["cidc_received"] = manifest_samples.cidc_received[0]
+        shipment["participant_count"] = manifest_samples.cimac_participant_id.nunique()
+        shipment["sample_count"] = manifest_samples.shape[0]
+
+        shipments.append(shipment)
+
+    return shipments
+
+
+shipments_dashboard = create_new_dashboard("upload_jobs")
+
+TRIAL_DROPDOWN = "trial-dropdown"
+SHIPMENTS_TABLE_ID = "shipments-table"
+MANIFEST_DROPDOWN = "manifest-dropdown"
+SAMPLES_TABLE_ID = "samples-table"
+
+shipments_dashboard.layout = html.Div(
+    [
+        dcc.Dropdown(
+            id=TRIAL_DROPDOWN, placeholder="Select trial to view shipment manifests"
+        ),
+        dt.DataTable(id=SHIPMENTS_TABLE_ID),
+        dcc.Dropdown(
+            id=MANIFEST_DROPDOWN, placeholder="Select manifest to view ingested samples"
+        ),
+        dt.DataTable(id=SAMPLES_TABLE_ID),
+    ]
+)
+
+
+@shipments_dashboard.callback(
+    Output(TRIAL_DROPDOWN, "options"), Input(TRIAL_DROPDOWN, "children")
+)
+@requires_auth("dash.shipments.trial_ids", [CIDCRole.ADMIN.value])
+def populate_trial_ids(trial_ids):
+    """Load available trial_ids into the trial id dropdown."""
+    if not trial_ids:
+        return [
+            {"label": t, "value": t} for t in TrialMetadata.get_distinct("trial_id")
+        ]
+    return trial_ids
+
+
+@shipments_dashboard.callback(
+    Output(MANIFEST_DROPDOWN, "options"), Input(TRIAL_DROPDOWN, "value")
+)
+@requires_auth("dash.shipments.manifest_ids", [CIDCRole.ADMIN.value])
+def populate_manifest_ids(trial_id):
+    """Load available manifest IDs into dropdown."""
+    # Don't populate the dropdown if no trial ID has been selected
+    if trial_id is None:
+        return []
+
+    shipments = get_trial_shipments(trial_id)
+    if shipments is None:
+        return []
+
+    return [
+        {"label": shipment["manifest_id"], "value": shipment["manifest_id"]}
+        for shipment in shipments
+    ]
+
+
+@shipments_dashboard.callback(
+    [Output(SHIPMENTS_TABLE_ID, "columns"), Output(SHIPMENTS_TABLE_ID, "data")],
+    [Input(TRIAL_DROPDOWN, "value")],
+)
+@requires_auth("dash.shipments.shipments", [CIDCRole.ADMIN.value])
+def get_shipments_for_trial(trial_id):
+    """Load successful upload jobs for the selected trials."""
+    columns = [
+        {"name": "Manifest Id", "id": "manifest_id"},
+        {"name": "Assay type", "id": "assay_type"},
+        {"name": "Date Shipped", "id": "date_shipped"},
+        {"name": "Date Recieved", "id": "date_received"},
+        {"name": "Date in CIDC", "id": "cidc_received"},
+        {"name": "# participants", "id": "participant_count"},
+        {"name": "# samples", "id": "sample_count"},
+    ]
+
+    if trial_id is None:
+        return [], []
+
+    shipments = get_trial_shipments(trial_id)
+    if shipments is None:
+        return columns, []
+
+    return columns, shipments
+
+
+@shipments_dashboard.callback(
+    [Output(SAMPLES_TABLE_ID, "columns"), Output(SAMPLES_TABLE_ID, "data")],
+    [Input(TRIAL_DROPDOWN, "value"), Input(MANIFEST_DROPDOWN, "value")],
+)
+@requires_auth("dash.shipments.samples", [CIDCRole.ADMIN.value])
+def get_samples_for_manifest(trial_id, manifest_id):
+    """Load successful upload jobs for the selected trials."""
+    columns = [
+        {"name": "Manifest Id", "id": "manifest_id"},
+        {"name": "CIMAC Id", "id": "cimac_id"},
+        {"name": "Cohort name", "id": "cohort_name"},
+        {"name": "Assay type", "id": "assay_type"},
+        {"name": "Sample Collection Procedure", "id": "sample_collection_procedure"},
+        {"name": "Type Of Sample", "id": "type_of_sample"},
+    ]
+
+    if trial_id is None or manifest_id is None:
+        return [], []
+
+    samples = get_manifest_samples(trial_id, manifest_id)
+    if samples is None:
+        return columns, []
+    return columns, samples.to_dict("records")
---
+++ tests/dashboards/test_shipments.py
@@ -0,0 +1,161 @@
+from datetime import datetime
+
+from dash.testing.composite import DashComposite
+from selenium.webdriver.common.by import By
+
+from cidc_api.models import (
+    Users,
+    UploadJobs,
+    ROLES,
+    UploadJobStatus,
+    CIDCRole,
+    TrialMetadata,
+)
+from cidc_api.dashboards.shipments import (
+    get_manifest_samples,
+    get_trial_shipments,
+    shipments_dashboard,
+    TRIAL_DROPDOWN,
+    SHIPMENTS_TABLE_ID,
+    MANIFEST_DROPDOWN,
+    SAMPLES_TABLE_ID,
+)
+
+from ..utils import make_role, mock_current_user
+
+
+trial_id = "test-trial"
+manifest_id = "test-manifest"
+assay_type = "CyTOF"
+num_samples = [2, 3, 5]
+num_participants = 3
+
+
+def setup_data(cidc_api, clean_db):
+    user = Users(email="test@email.com", approval_date=datetime.now())
+    shipment = {
+        "courier": "FEDEX",
+        "ship_to": "",
+        "ship_from": "",
+        "assay_type": assay_type,
+        "manifest_id": manifest_id,
+        "date_shipped": "2020-06-10 00:00:00",
+        "date_received": "2020-06-11 00:00:00",
+        "account_number": "",
+        "assay_priority": "1",
+        "receiving_party": "MSSM_Rahman",
+        "tracking_number": "",
+        "shipping_condition": "Frozen_Dry_Ice",
+        "quality_of_shipment": "Specimen shipment received in good condition",
+    }
+    metadata = {
+        "protocol_identifier": trial_id,
+        "shipments": [
+            # we get duplicate shipment uploads sometimes
+            shipment,
+            shipment,
+        ],
+        "participants": [
+            {
+                "cimac_participant_id": f"CTTTPP{p}",
+                "participant_id": "x",
+                "cohort_name": "",
+                "samples": [
+                    {
+                        "cimac_id": f"CTTTPP{p}SS.0{s}",
+                        "sample_location": "",
+                        "type_of_primary_container": "Other",
+                        "type_of_sample": "Other",
+                        "collection_event_name": "",
+                        "parent_sample_id": "",
+                    }
+                    for s in range(num_samples[p])
+                ],
+            }
+            for p in range(num_participants)
+        ],
+        "allowed_cohort_names": [""],
+        "allowed_collection_event_names": [""],
+    }
+    trial = TrialMetadata(trial_id=trial_id, metadata_json=metadata)
+    upload_job = UploadJobs(
+        uploader_email=user.email,
+        trial_id=trial.trial_id,
+        upload_type="pbmc",
+        gcs_xlsx_uri="",
+        metadata_patch=metadata,
+        multifile=False,
+    )
+    upload_job._set_status_no_validation(UploadJobStatus.MERGE_COMPLETED.value)
+    with cidc_api.app_context():
+        user.insert()
+        trial.insert()
+        upload_job.insert()
+
+        clean_db.refresh(user)
+        clean_db.refresh(upload_job)
+        clean_db.refresh(trial)
+
+    return user, upload_job, trial
+
+
+def test_shipments_dashboard(cidc_api, clean_db, monkeypatch, dash_duo: DashComposite):
+    """
+    Check that the shipments dashboard behaves as expected.
+    """
+    user, _, _ = setup_data(cidc_api, clean_db)
+
+    for role in ROLES:
+        make_role(user.id, role, cidc_api)
+        mock_current_user(user, monkeypatch)
+
+        dash_duo.server(shipments_dashboard)
+        dash_duo.wait_for_page(f"{dash_duo.server.url}/dashboards/upload_jobs/")
+
+        if CIDCRole(role) == CIDCRole.ADMIN:
+            # open trial dropdown
+            dash_duo.click_at_coord_fractions(f"#{TRIAL_DROPDOWN}", 0.1, 0.1)
+            dash_duo.wait_for_contains_text(f"#{TRIAL_DROPDOWN}", trial_id)
+            # select the first trial
+            trial_select = dash_duo.find_elements(".VirtualizedSelectOption")[0]
+            dash_duo.click_at_coord_fractions(trial_select, 0.1, 0.1)
+            # click off dropdown to close it
+            dash_duo.click_at_coord_fractions(f"#{SHIPMENTS_TABLE_ID}", 0.1, 0.1)
+            # ensure the shipments table loads
+            dash_duo.wait_for_contains_text(f"#{SHIPMENTS_TABLE_ID}", manifest_id)
+            # open manifest dropdown
+            dash_duo.click_at_coord_fractions(f"#{MANIFEST_DROPDOWN}", 0.1, 0.1)
+            dash_duo.wait_for_contains_text(f"#{TRIAL_DROPDOWN}", trial_id)
+            # select the first manifest
+            manifest = dash_duo.find_elements(".VirtualizedSelectOption")[0]
+            dash_duo.click_at_coord_fractions(manifest, 0.1, 0.1)
+            # ensure the samples table loads
+            dash_duo.wait_for_contains_text(f"#{SAMPLES_TABLE_ID}", "CTTTPP1SS.01")
+        else:
+            dash_duo._wait_for_callbacks()
+            assert any(
+                ["401 (UNAUTHORIZED)" in log["message"] for log in dash_duo.get_logs()]
+            )
+
+
+def test_get_manifest_samples(cidc_api, clean_db):
+    """Test the helper function used for getting a list of samples for a given trial manifest."""
+    setup_data(cidc_api, clean_db)
+
+    with cidc_api.app_context():
+        samples = get_manifest_samples(trial_id, manifest_id)
+        assert samples.cimac_id.nunique() == sum(num_samples)
+        assert samples.cimac_participant_id.nunique() == num_participants
+
+
+def test_get_trial_shipments(cidc_api, clean_db):
+    """Test the helper function used for getting a list of shipments for a given trial."""
+    _, upload_job, _ = setup_data(cidc_api, clean_db)
+
+    with cidc_api.app_context():
+        shipments = get_trial_shipments(trial_id)
+        assert len(shipments) == 1
+        shipment = shipments[0]
+        assert shipment["cidc_received"] == upload_job._created
+        assert shipment["participant_count"] == num_participants
+        assert shipment["sample_count"] == sum(num_samples)
