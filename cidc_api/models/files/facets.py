7554f27612fdd3bd942d3805af300da5d25801ef
Roshni <rbiswas@ds.dfci.harvard.edu>
2021-03-19 00:39:21 +0530

    fix hande facet string

--- cidc_api/models/files/facets.py
+++ cidc_api/models/files/facets.py
@@ -219,7 +219,7 @@ assay_facets: Facets = {
         "Images": FacetConfig(["/ihc/ihc_image."]),
         "Combined Markers": FacetConfig(["csv|ihc marker combined"]),
     },
-    "H&E": {"Images": FacetConfig(["hande/image_file.svs"], "Stained image files.")},
+    "H&E": {"Images": FacetConfig(["/hande/image_file.svs"], "Stained image file.")},
     "TCR": {
         "Source": FacetConfig(
             [
