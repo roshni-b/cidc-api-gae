3ad0b3c9c53dfea1787b57ed9f184c924a6ac608
Jacob Lurye <jlurye@jimmy.harvard.edu>
2021-05-03 10:58:30 -0400

    Update gunicorn and app engine configs to improve performance (#470)
    * Use async gunicorn workers
    
    * gunicorn and app engine config tweaks
    
    * Update app engine configs to autoscale less eagerly

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,25 @@
 """Configuration for the gunicorn WSGI server."""
 import os
-from gunicorn.arbiter import Arbiter
 
-is_dev = os.environ.get("ENV") == "dev"
+from gevent.monkey import patch_all
+from psycogreen.gevent import patch_psycopg
 
+# The "gevent" worker class that we select below uses
+# greenlets under the hood. Greenlets monkeypatch I/O
+# funcionality to support async-io.
+# gevent docs: http://www.gevent.org/
+patch_all()
+patch_psycopg()  # our postgres db driver needs to be patched directly
+
+# Use async workers: https://docs.gunicorn.org/en/stable/design.html#async-workers
+worker_class = "gevent"
 # See https://docs.gunicorn.org/en/stable/settings.html
 port = os.environ.get("PORT", 8080)
-loglevel = "DEBUG" if is_dev else "INFO"
-reload = is_dev
+# Development mode - restart the server on code changes
+reload = os.environ.get("ENV") == "dev"
+# Include debug level-logs if we're in development mode
+loglevel = "DEBUG" if reload else "INFO"
+# Cancel ongoing requests after 500 seconds of waiting
 timeout = 500
 # Send all logs to stdout (where App Engine reads them from)
 errorlog = "-"
