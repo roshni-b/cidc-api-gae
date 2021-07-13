c364d7b8ea91cb221226581c1dea934c9cafc0b3
SC Van Nostrand <scvannost@gmail.com>
2021-07-13 11:43:50 -0400

    Update settings.py

--- cidc_api/config/settings.py
+++ cidc_api/config/settings.py
@@ -106,7 +106,9 @@ CSMS_TOKEN_URL = environ.get("CSMS_TOKEN_URL")
 if not TESTING:
     secret_manager = get_secrets_manager()
     # CSMS_CLIENT_SECRET = secret_manager.get("CSMS_CLIENT_SECRET")
+    CSMS_CLIENT_SECRET = "foo"
     # CSMS_CLIENT_ID = secret_manager.get("CSMS_CLIENT_ID")
+    CSMS_CLIENT_ID = "cidc"
 else:
     CSMS_CLIENT_SECRET = environ.get("CSMS_CLIENT_SECRET")
     CSMS_CLIENT_ID = environ.get("CSMS_CLIENT_ID")
