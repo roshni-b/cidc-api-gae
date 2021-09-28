ef2a9aa188730ea40aa6d4736d2c940790f04f4f
Roshni Biswas <rbiswas@ds.dfci.harvard.edu>
2021-09-28 22:12:53 +0530

    CIDC-1149 replace range(len() w enumerate()

--- tests/csms/data.py
+++ tests/csms/data.py
@@ -649,7 +649,7 @@ manifests = [
     for manifest in _manifests_to_make
 ]
 
-for n in range(len(manifests)):
+for n in enumerate(manifests):
     manifests[n]["samples"] = [
         s for s in samples if s.get("manifest_id") == manifests[n]["manifest_id"]
     ]
